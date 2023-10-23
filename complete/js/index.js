3d8edc454af0e1b7e2b32156bc3cac87a960de6a
tomy-web-cordinator <132113380+tomy-web-coordinator@users.noreply.github.com>
2023-10-24 08:39:03 +0900

    Add files via upload

---
+++ complete/js/index.js
@@ -0,0 +1,6 @@
+// ハンバーガーボタンとドロワー
+$("#js-button-drawer").on("click", function () {
+  $(this).toggleClass("is-checked");
+  $("#js-drawer").slideToggle();
+  $("body").toggleClass("is-fixed");
+});
---
+++ js/index.js
@@ -0,0 +1,5 @@
+$("#js-button-drawer").on("click", function () {
+  $(this).toggleClass("is-checked");
+  $("#js-drawer").slideToggle();
+  $("body").toggleClass("is-fixed");
+});
